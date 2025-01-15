2b6a5479de0a48d1e09dfa8cb8405d56b2fa494d
Merik Nugroho <mericknugroho@gmail.com>
2025-01-15 16:56:08 +0700

    tambahan kedua, css dll

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate laborum omnis inventore, asperiores, reiciendis eaque facilis nostrum repellat earum quidem qui voluptates aliquid nesciunt, delectus nobis nam velit laudantium aperiam.
+</body>
+</html>
